78e00fd550a7a0672dce9d9570d26a073afb32d2
Peter Butzhammer <peter.butzhammer@evopro-ag.de>
2020-07-11 10:03:05 +0200

    Add unit test project

---
+++ PersistentQueue.Tests/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PersistentQueue.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void METHOD()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+            Assert.Fail();
+        }
+
+    }
+}
