962fe88dd530919250906ff347dd3950cb3b7397
Peter Butzhammer <peter@butzhammer.de>
2020-07-13 21:02:43 +0200

    Add benchmark project

---
+++ PersistentQueue.Benchmarks/Commons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Persistent.Queue.Benchmarks
+{
+    public static class Commons
+    {
+        public static string GetTempPath()
+        {
+            return Path.Combine(Path.GetTempPath(), "PersistentQueue.Benchmarks", Guid.NewGuid().ToString());
+        }
+
+    }
+}
